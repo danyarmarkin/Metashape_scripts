933fa9ebf3b923032249051b9705b84a569c093c
Danila Yarmarkin <danila.iarmarkin@gmail.com>
2021-05-31 18:53:58 +0300

    add start file

---
+++ scripts.py
@@ -0,0 +1,18 @@
+import scaling
+import pivot
+try:
+    import Metashape
+except:
+    print("Cant find Metashape")
+
+
+
+label_scaling = "Scripts/Scaling"
+label_pivot = "Scripts/Pivot"
+try:
+    Metashape.app.removeMenuItem(label_scaling)
+    Metashape.app.removeMenuItem(label_pivot)
+except:
+    pass
+Metashape.app.addMenuItem(label_scaling, scaling.scale)
+Metashape.app.addMenuItem(label_pivot, pivot.setPivot)
