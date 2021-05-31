3e9d89346986c18ab99c5ad9d1ece18f11193d74
Danila Yarmarkin <danila.iarmarkin@gmail.com>
2021-06-01 00:06:13 +0300

    new keyboard shortcut for scaling.py

--- scaling.py
+++ scaling.py
@@ -56,5 +56,5 @@ def init():
         Metashape.app.removeMenuItem(label)
     except:
         pass
-    Metashape.app.addMenuItem(label, scale, "Ctrl + M")
+    Metashape.app.addMenuItem(label, scale, "CtrlM")
     print("To execute this script press {}".format(label))
