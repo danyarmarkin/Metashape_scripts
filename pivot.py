162bfc508824082a80736ebd468f82efdb20c5c1
danyarmarkin <58003908+danyarmarkin@users.noreply.github.com>
2021-06-07 20:54:46 +0300

    Update pivot.py
    a

--- pivot.py
+++ pivot.py
@@ -13,26 +13,25 @@ def setPivot():
     app = Metashape.app
     doc = app.document
     chunk = doc.chunk
-    # point_cloud = chunk.point_cloud
-    # print("got point cloud")
-    # points = point_cloud.points
-    # print("got points")
-    # x_sum = 0
-    # y_sum = 0
-    # z_sum = 0
-    # for i in range(0, len(points), 10):
-    #     x_sum += points[i].coord.x
-    #     y_sum += points[i].coord.y
-    #     z_sum += points[i].coord.z
-    # c = len(points) // 10
-    # x = x_sum / c
-    # y = y_sum / c
-    # z = z_sum / c
-    # print(x, y, z)
-    # vc = Metashape.Vector([x, y, z])
-    # vc.w = 1
-    # print(vc)
-    chunk.transform.translation = Metashape.Vector([0, 0, 0])
+    point_cloud = chunk.point_cloud
+    print("got point cloud")
+    points = point_cloud.points
+    print("got points")
+    x_sum = 0
+    y_sum = 0
+    z_sum = 0
+    for i in range(0, len(points), 10):
+        x_sum += points[i].coord.x
+        y_sum += points[i].coord.y
+        z_sum += points[i].coord.z
+    c = len(points) // 10
+    x = x_sum / c
+    y = y_sum / c
+    z = z_sum / c
+    print(x, y, z)
+    vc = Metashape.Vector([-x, -y, -z])
+    print(vc)
+    chunk.transform.translation = -vc
     print("finish script")
 
 def init():
